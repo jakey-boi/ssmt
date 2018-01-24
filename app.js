24b4764cbd0a3c41e3b5e17a38f02b87376746fb
MikeModder <15313620+MikeModder@users.noreply.github.com>
2018-01-23 19:57:03 -0500

    Admin page and environments

--- app.js
+++ app.js
@@ -12,40 +12,43 @@ const config = require('./config.json');
 
 const app = express();
 
+//console.log(config.dev);
+
 /* SETUP PASSWORDLESS */
 passwordless.init(new pwlStore(`${__dirname}/data/pwl.db`));
 passwordless.addDelivery((token, uid, recipient, cb, req) => {
-    let host = 'vps.unsafe.men';
+    let host = config.host;
     let msg = {
         to: recipient,
         from: 'no-reply@vps.unsafe.men',
         subject: 'SSMT Login Token',
         html: `Here's your login URL: <a href="http://${host}/?token=${token}&uid=${encodeURIComponent(uid)}">http://${host}/?token=${token}&uid=${encodeURIComponent(uid)}</a>`
     }
-    email.send(msg)
-        .catch(e => {
-            console.log(`Error sending email! ${e}`);
-            cb(e);
-        })
-        .then(() => {
-            cb(null);
-        });
-
-    app.locals.db.get('SELECT * FROM users WHERE email = ?', uid, (err, row) => {
-        if(err) throw err;
-        if(row){
-            //do nothing
-        } else {
-            app.locals.db.run('INSERT INTO users (username, email, joinedAt) VALUES (?, ?, ?)', uid, uid,  Date.now(), (err) => {
-                if(err) throw err;
+    if(config.dev){
+        console.log(`http://${host}/?token=${token}&uid=${encodeURIComponent(uid)}`);
+        cb(null);
+    } else {
+        email.send(msg)
+            .catch(e => {
+                console.log(`Error sending email! ${e}`);
+                cb(e);
             })
-        }
-    })
-    //console.log(`http://localhost:8080/?token=${token}&uid=${encodeURIComponent(uid)}`);
-});
+            .then(() => {
+                cb(null);
+            });
 
-/* SETUP SENDGRID */
-email.setApiKey(config.sendgrid.apikey);
+        app.locals.db.get('SELECT * FROM users WHERE email = ?', uid, (err, row) => {
+            if(err) throw err;
+            if(row){
+                //do nothing
+            } else {
+                app.locals.db.run('INSERT INTO users (username, email, joinedAt) VALUES (?, ?, ?)', uid, uid,  Date.now(), (err) => {
+                    if(err) throw err;
+                })
+            }
+        })
+    }
+});
 
 /* STATIC FILES */
 app.use(express.static('public'));
@@ -61,17 +64,28 @@ app.use(passwordless.acceptToken({ successRedirect: '/' }));
 const indexRoute = require('./routes/index.js');
 const postsRoute = require('./routes/posts.js');
 const userRoute = require('./routes/user.js');
+const adminRoute = require('./routes/admin.js');
 
 app.use('/', indexRoute);
 app.use('/posts', postsRoute);
 app.use('/user', userRoute);
+app.use('/admin', adminRoute);
 
 /* VIEW ENGINE AND FOLDER */
 app.set('view engine', 'ejs');
 app.set('views', `${__dirname}/views`);
 
-/* DATABASE */
+/* DATABASE & CONFIG */
 app.locals.db = new sqlite3.Database(`${__dirname}/data/ssmt.db`);
+app.locals.config = require('./config.json');
+
+/* SETUP SENDGRID */
+email.setApiKey(config.sendgrid.apikey);
+
+/* 404 */
+app.get('*', (req, res) => {
+    res.render('error/404');
+});
 
 /* LISTEN */
 app.listen(config.port, () => {
---
+++ routes/admin.js
@@ -0,0 +1,39 @@
+const router = require('express').Router();
+const passwordess = require('passwordless');
+
+router.get('/', passwordess.restricted(), (req, res) => {
+    let owner = req.app.locals.config.owner;
+    if(req.user !== owner) return res.redirect('/');
+    res.render('admin/home');
+});
+
+router.post('/eval', passwordess.restricted(), (req, res) => {
+    let owner = req.app.locals.config.owner;
+    if(req.user !== owner) return res.redirect('/');
+    let code = req.body.code;
+
+    switch(req.body.type){
+        case 'js':
+            //javascript
+            try{
+                let result = eval(code);
+                res.json({ success: true, out: result });
+            } catch (err){
+                res.json({ success: false, out: err });
+            }
+            break;
+        case 'sql':
+            //sql
+            req.app.locals.db.all(code, (err, rows) => {
+                if(err) res.json({ success: false, out: err });
+                res.json({ success: true, out: JSON.stringify(rows) });
+            });
+            break;
+        default:
+            //fail
+            res.json({ success: false, out: 'A code type was not specified!' });
+            break;
+    }
+});
+
+module.exports = router;
